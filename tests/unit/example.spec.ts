6a0f98ab0a7b516e0c49d17428de3df085f5fd5f
Andrei Chirkov <aachirkov@me.com>
2022-08-12 18:06:21 +0300

    Test with axios mock, async ang suspense

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,17 +1,45 @@
 import {nextTick} from "vue";
-import {mount} from "@vue/test-utils";
+import {mount, flushPromises} from "@vue/test-utils";
 import Timeline from "@/components/Timeline.vue";
 import {today, thisWeek, thisMonth} from "@/mocks";
 
+//Передача нод модуля
+//Делаем кастомную имплементацию
+jest.mock('axios', () => ({
+  get: (url: string) => {
+    return Promise.resolve({
+      data: [today, thisWeek, thisMonth]
+    })
+  }
+}))
+
+function mountTimeline() {
+  return mount({
+    components: {Timeline},
+    template: `
+        <suspense>
+          <template #default>
+            <timeline />
+          </template>
+          <template #fallback>
+            Loading...
+          </template>
+        </suspense>
+      `
+  })
+}
+
 describe('Timeline', () => {
-  it('renders today post default', () => {
-    const wrapper = mount(Timeline)
+  it('renders today post default', async () => {
+    const wrapper = mountTimeline()
+    await flushPromises()
 
     expect(wrapper.html()).toContain(today.created.format('Do MMM'))
   })
 
   it('update when click on period btn', async () => {
-    const wrapper = mount(Timeline)
+    const wrapper = mountTimeline()
+    await flushPromises()
 
     await wrapper.get('[data-test="This Week"]').trigger('click') //nextTick
 
@@ -20,7 +48,8 @@ describe('Timeline', () => {
   })
 
   it('update when click on period btn', async () => {
-    const wrapper = mount(Timeline)
+    const wrapper = mountTimeline()
+    await flushPromises()
 
     await wrapper.get('[data-test="This Month"]').trigger('click') //nextTick
 
