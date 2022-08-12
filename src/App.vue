281331ff2bf8f81b3dc1386a7ad8b129eba0de06
Andrei Chirkov <aachirkov@me.com>
2022-08-12 13:40:15 +0300

    Add spinner

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <section class="section">
     <div class="container">
       <suspense>
-        <timeline />
+        <template #default>
+          <timeline />
+        </template>
+        <template #fallback>
+          <spinner />
+        </template>
       </suspense>
     </div>
   </section>
@@ -11,10 +16,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Timeline from './components/Timeline.vue';
+import Spinner from "@/components/Spinner.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
+    Spinner,
     Timeline
   }
 });
---
+++ src/components/Spinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <progress class="progress is-primary is-small"></progress>
+</template>
+
+<script>
+export default {
+  name: "Spinner"
+}
+</script>
+
+<style scoped>
+
+</style>
